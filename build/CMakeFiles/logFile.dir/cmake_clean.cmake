ac8c254d82724c9beb1d522a76f9d454dda737e1
sssh <shihar1994@gmail.com>
2025-01-23 08:58:59 +0530

    First commit

---
+++ build/CMakeFiles/logFile.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logFile.dir/main.cpp.o"
+  "CMakeFiles/logFile.dir/main.cpp.o.d"
+  "logFile"
+  "logFile.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logFile.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
